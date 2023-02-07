f171ebefcd3422642aae01faecda105f208d9e80
Serg Skol <sps676@gmail.com>
2023-02-07 11:43:36 +0100

    Add decorator

--- person.rb
+++ person.rb
@@ -1,5 +1,8 @@
-class Person
-  def initialize(age, name: 'Unknown', parent_permission: true)
+require_relative 'nameable'
+
+class Person < Nameable
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -10,11 +13,14 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  private
+  def correct_name
+    @name
+  end
 
   def of_age?
     @age >= 18
   end
+  private :of_age?
 
   def can_use_services?
     if @parent_permission || of_age

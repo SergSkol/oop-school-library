82e26dc5eab9c4ee8ddb2816b8d70a7aa75f648d
Serg Skol <sps676@gmail.com>
2023-02-13 10:16:40 +0100

    Refactoring by SOLID

--- app.rb
+++ app.rb
@@ -65,7 +65,7 @@ class App
     @books.each_with_index do |e, index|
       puts "#{index}) Title: #{e.title}, Author: #{e.author}"
     end
-    book_number = gets.chomp
+    book_number = gets.chomp.to_i
 
     puts 'Select a person from the following list by number (not id)'
     @people.each_with_index do |e, index|
@@ -90,40 +90,5 @@ class App
     @rentals.each do |e|
       puts "Date: #{e.date}, Book: #{e.book.title} by #{e.book.author}" if e.person.id == id
     end
-  end
-
-  def menu
-    puts
-    puts 'Please choose an option by entering a number'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given persion id'
-    puts '7 - Exit'
-  end
-
-  def run
-    loop do
-      menu
-      option = gets.chomp.to_i
-      case option
-      when 1
-        list_all_books
-      when 2
-        list_all_people
-      when 3
-        create_person
-      when 4
-        create_book
-      when 5
-        create_rental
-      when 6
-        list_rentals
-      else break
-      end
-    end
-    puts 'Thank you for using this app!'
   end
 end
